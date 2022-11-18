50461338e153beead183426bc8aa792cf147c9cf
FabioTw <e.cherwoo@gmail.com>
2022-11-18 17:27:56 +0300

    fix: ecosystem.config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 
 const {
@@ -10,18 +9,20 @@ const {
   POSTGRES_PGDATA,
 } = process.env;
 
-export const apps = [
-  {
-    name: 'kupipodariday-backend',
-    script: './dist/main.js',
-    env_env_production: {
-      NODE_ENV: 'production',
-      JWT_SECRET,
-      POSTGRES_PASSWORD,
-      POSTGRES_DB,
-      POSTGRES_HOST,
-      POSTGRES_USER,
-      POSTGRES_PGDATA,
+module.exports = {
+  apps: [
+    {
+      name: 'kupipodariday-backend',
+      script: './dist/main.js',
+      env_env_production: {
+        NODE_ENV: 'production',
+        JWT_SECRET,
+        POSTGRES_PASSWORD,
+        POSTGRES_DB,
+        POSTGRES_HOST,
+        POSTGRES_USER,
+        POSTGRES_PGDATA,
+      },
     },
-  },
-];
+  ],
+};
